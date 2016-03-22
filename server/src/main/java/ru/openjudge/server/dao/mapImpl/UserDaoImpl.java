5583b658d55362b6ee4e4aced64350dd4347a9eb
kvark161 <kvark161@gmail.com>
2016-03-22 14:27:02 +0300

    generate User id in map's UserDaoImpl

--- server/src/main/java/ru/openjudge/server/dao/mapImpl/UserDaoImpl.java
+++ server/src/main/java/ru/openjudge/server/dao/mapImpl/UserDaoImpl.java
@@ -5,25 +5,32 @@ import ru.openjudge.server.dao.DaoException;
 import ru.openjudge.server.dao.UserDao;
 import ru.openjudge.server.entity.User;
 
+import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.StringJoiner;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class UserDaoImpl implements UserDao {
 
     private ConcurrentMap<Long, User> data;
+    private AtomicLong maxId;
 
     public UserDaoImpl() {
         data = new ConcurrentHashMap<>();
+        maxId.set(0);
         {
             User admin = new User("admin", "admin");
-            data.put(0L, admin);
+            data.put(1L, admin);
         }
     }
 
     @Override
     public void save(User user) {
+        if (data.get(user.getId()) == null) {
+            generateUserId(user);
+        }
         data.put(user.getId(), user);
     }
 
@@ -50,4 +57,18 @@ public class UserDaoImpl implements UserDao {
         }
         throw new DaoException(String.format("User with login = \"%s\" not found", login));
     }
+
+    private void generateUserId(User user) {
+        Field field;
+        Long newId = maxId.get();
+        maxId.set(maxId.get() + 1);
+        try {
+            field = user.getClass().getDeclaredField("id");
+            field.setAccessible(true);
+            field.set(user, newId);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
