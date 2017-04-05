83894b11e0c28f1d4c7e83ab1b81dd27fb160c7f
kvark161 <kvark161@yandex.ru>
2017-04-05 13:48:48 +0300

    add routing

--- eskimo-server/eskimo-frontend/src/main/eskimo-frontend/src/app/app.module.ts
+++ eskimo-server/eskimo-frontend/src/main/eskimo-frontend/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import {MaterialModule} from "@angular/material";
+import {AboutComponent} from "./pages/about/about.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {router} from "./app.router";
+import {RouterModule} from "@angular/router";
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    RouterModule.forRoot(router)
+  ],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ eskimo-server/eskimo-frontend/src/main/eskimo-frontend/src/app/app.router.ts
@@ -0,0 +1,9 @@
+import {Routes} from "@angular/router";
+import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
+
+export const router: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent}
+];
