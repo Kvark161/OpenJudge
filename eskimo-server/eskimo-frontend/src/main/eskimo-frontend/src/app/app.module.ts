73599ae421182babe0edf5f85733f1650c299c3f
kvark161 <kvark161@yandex.ru>
2017-04-06 11:25:40 +0300

    fix pom and home.component

--- eskimo-server/eskimo-frontend/src/main/eskimo-frontend/src/app/app.module.ts
+++ eskimo-server/eskimo-frontend/src/main/eskimo-frontend/src/app/app.module.ts
@@ -2,16 +2,15 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {RouterModule} from "@angular/router";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
+import {router} from "./app.router";
+import {EskimoService} from "./services/eskimo.service";
+import {ContestsComponent} from "./pages/contests/contests.component";
 import {AppComponent} from "./app.component";
 import {AboutComponent} from "./pages/about/about.component";
 import {HomeComponent} from "./pages/home/home.component";
-import {router} from "./app.router";
-import {RouterModule} from "@angular/router";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import {EskimoService} from "./services/eskimo.service";
-import {ContestsComponent} from "./pages/contests/contests.component";
-
 
 @NgModule({
     imports: [
