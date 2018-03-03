2e8d275c18fca8a092528c822f500db727f8fd69
kvark161 <kvark161@yandex.ru>
2018-03-04 01:26:53 +0300

    fix 404 error on update page

--- server/backend/src/main/java/eskimo/backend/BackendApp.java
+++ server/backend/src/main/java/eskimo/backend/BackendApp.java
@@ -5,15 +5,20 @@ import eskimo.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
 @SpringBootApplication
 @EnableConfigurationProperties
-@EnableWebMvc
 public class BackendApp extends WebMvcConfigurerAdapter {
 
     public static void main(String... args) {
@@ -30,5 +35,17 @@ public class BackendApp extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor(userService));
+    }
+
+    @Bean
+    ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return new ErrorViewResolver() {
+            @Override
+            public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+                return status == HttpStatus.NOT_FOUND
+                        ? new ModelAndView("index.html", Collections.<String, Object>emptyMap(), HttpStatus.OK)
+                        : null;
+            }
+        };
     }
 }
