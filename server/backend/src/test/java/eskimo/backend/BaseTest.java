6d2beb5affaf5414366175d78e46aa3089959bc2
kvark161 <kvark161@yandex.ru>
2018-03-17 15:40:35 +0300

    clean resource before each test

---
+++ server/backend/src/test/java/eskimo/backend/BaseTest.java
@@ -0,0 +1,34 @@
+package eskimo.backend;
+
+import eskimo.backend.config.AppSettings;
+import org.apache.commons.io.FileUtils;
+import org.flywaydb.core.Flyway;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
+public class BaseTest {
+
+    @Autowired
+    private Flyway flyway;
+
+    @Autowired
+    private AppSettings appSettings;
+
+    @Before
+    public void clean() throws IOException {
+        FileUtils.deleteDirectory(appSettings.getStoragePath());
+        FileUtils.deleteDirectory(appSettings.getTempPath());
+        flyway.clean();
+        flyway.migrate();
+    }
+
+}
--- server/backend/src/test/java/eskimo/backend/dao/ContestDaoTest.java
+++ server/backend/src/test/java/eskimo/backend/dao/ContestDaoTest.java
@@ -1,21 +1,17 @@
 package eskimo.backend.dao;
 
+import eskimo.backend.BaseTest;
 import eskimo.backend.entity.Contest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ContestDaoTest {
+public class ContestDaoTest extends BaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(ContestDao.class);
 
